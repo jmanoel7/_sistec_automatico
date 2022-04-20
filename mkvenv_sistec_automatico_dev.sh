62fbe1d2fd5f1e650b994ce53a1109ef0142538d
Joao Manoel <joaomanoel.ifg@gmail.com>
2022-04-20 14:21:49 -0300

    atualização do código virtualenv ...

--- mkvenv_sistec_automatico_dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p /srv/www/sistec_automatico/venv/
-virtualenv -p /usr/bin/python3.9 /srv/www/sistec_automatico/venv/sistec_automatico_dev
-source /srv/www/sistec_automatico/venv/sistec_automatico_dev/bin/activate
-cd /srv/www/sistec_automatico/
-pip install -U -r requirements.dev.txt
-exit 0
